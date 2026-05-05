ac9ddc6475b6a6b94791bb1c782215de1372e73c
김용권 <yongkwon.kim@ikoob.com>
2026-05-06 08:52:57 +0900

    refactor : 빈 이니셜라이저로 클래스 분리

--- src/main/java/com/diy/app/LectureApplication.java
+++ src/main/java/com/diy/app/LectureApplication.java
@@ -1,49 +1,17 @@
 package com.diy.app;
 
-import com.diy.framework.web.beans.annotations.Autowired;
-import com.diy.framework.web.beans.annotations.Component;
+import com.diy.framework.web.beans.factory.BeanInitializer;
 import com.diy.framework.web.beans.factory.BeanScanner;
 import com.diy.framework.web.mvc.Controller;
 import com.diy.framework.web.server.TomcatWebServer;
 import com.diy.framework.web.servlet.DispatcherServlet;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class LectureApplication {
-    public static void main(String[] args)
-        throws InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws Exception {
         BeanScanner beanScanner = new BeanScanner("com.diy.app");
-
-        Map<Class<?>, Object> map = new HashMap<>();
-
-        Set<Class<?>> classes = beanScanner.scanClassesTypeAnnotatedWith(Component.class);
-        for (Class<?> clazz : classes) {
-            Constructor<?>[] declaredConstructors = clazz.getDeclaredConstructors();
-            for (Constructor<?> constructor : declaredConstructors) {
-                if(!constructor.isAnnotationPresent(Autowired.class)) {
-                    map.put(clazz, constructor.newInstance());
-                }
-            }
-        }
-
-        for (Class<?> clazz : classes) {
-            Constructor<?>[] declaredConstructors = clazz.getDeclaredConstructors();
-            for (Constructor<?> constructor : declaredConstructors) {
-                if(constructor.isAnnotationPresent(Autowired.class)) {
-                    List<Object> list = new ArrayList<>();
-                    Class<?>[] parameterTypes = constructor.getParameterTypes();
-                    for (Class<?> parameterType : parameterTypes) {
-                        list.add(map.get(parameterType));
-                    }
-                    map.put(clazz, constructor.newInstance(list.toArray()));
-                }
-            }
-        }
+        BeanInitializer beanInitializer = new BeanInitializer(beanScanner);
 
         final Map<String, Controller> controllerMapping = new HashMap<>();
         controllerMapping.put("/lectures", new LectureController());
---
+++ src/main/java/com/diy/framework/web/beans/factory/BeanInitializer.java
@@ -0,0 +1,42 @@
+package com.diy.framework.web.beans.factory;
+
+import com.diy.framework.web.beans.annotations.Autowired;
+import com.diy.framework.web.beans.annotations.Component;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class BeanInitializer {
+
+    private final Map<Class<?>, Object> beans = new HashMap<>();
+
+    public BeanInitializer(BeanScanner scanner)
+        throws Exception {
+        Set<Class<?>> classes = scanner.scanClassesTypeAnnotatedWith(Component.class);
+        for (Class<?> clazz : classes) {
+            Constructor<?>[] declaredConstructors = clazz.getDeclaredConstructors();
+            for (Constructor<?> constructor : declaredConstructors) {
+                if (!constructor.isAnnotationPresent(Autowired.class)) {
+                    beans.put(clazz, constructor.newInstance());
+                }
+            }
+        }
+
+        for (Class<?> clazz : classes) {
+            Constructor<?>[] declaredConstructors = clazz.getDeclaredConstructors();
+            for (Constructor<?> constructor : declaredConstructors) {
+                if (constructor.isAnnotationPresent(Autowired.class)) {
+                    List<Object> list = new ArrayList<>();
+                    Class<?>[] parameterTypes = constructor.getParameterTypes();
+                    for (Class<?> parameterType : parameterTypes) {
+                        list.add(beans.get(parameterType));
+                    }
+                    beans.put(clazz, constructor.newInstance(list.toArray()));
+                }
+            }
+        }
+    }
+}
